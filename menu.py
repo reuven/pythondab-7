16602497faab7a08ff290320eee2a6fc5831a000
Ben Bar-Or <benbaror.galt@gmail.com>
2025-08-10 09:38:37 +0200

    updated doc strings

--- menu.py
+++ menu.py
@@ -7,6 +7,19 @@ It contains one function, menu.
 import sys
 
 def menu(*args):
+	"""
+	Present a list of string options to the user and prompt for a choice.
+
+	Repeatedly shows the available options and asks the user to select one.
+	If the user imput is not among the option, it asks again until a valid choice is made.
+
+	Args:
+		*args: Any number of string options to choose from.
+
+	Returns:
+		The chosen string from the provided options.
+	"""
+
 	while True:
 		print('Available options are:')
 		for arg in args:
